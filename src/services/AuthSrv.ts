e60809f313f36ffe2f7e9640089d832673fdba72
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2022-08-03 23:11:28 +0700

    feat: wrap cookie functionality into auth srv

--- src/services/AuthSrv.ts
+++ src/services/AuthSrv.ts
@@ -1,5 +1,3 @@
-import { setCookie, getCookie } from '../utils';
-
 class AuthSrv {
   constructor() {}
 
@@ -8,11 +6,11 @@ class AuthSrv {
   }
 
   public setToken(token: string) {
-    setCookie('token', token, 1);
+    this._setCookie('token', token, 1);
   }
 
   public getToken() {
-    return getCookie('token');
+    return this._getCookie('token');
   }
 
   public isAuthenticated(): boolean {
@@ -26,6 +24,30 @@ class AuthSrv {
   public deleteAuthInfo() {
     localStorage.removeItem('is_admin');
     this.setToken('');
+  }
+
+  private _setCookie(cname: string, cvalue: string, exdays: number) {
+    const d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    let expires = 'expires=' + d.toUTCString();
+    document.cookie =
+      cname + '=' + cvalue + ';' + expires + ';path=/';
+  }
+
+  private _getCookie(cname: string) {
+    let name = cname + '=';
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return null;
   }
 }
 
